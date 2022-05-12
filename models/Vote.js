7955a7fc13c471d5a8d76299dc9c11ffd7d9fe07
TaimurHasan <taimurhasan11@gmail.com>
2022-05-11 23:08:59 -0400

    added vote routes and api endpoints and allowed users to vote on posts

--- models/Vote.js
+++ models/Vote.js
@@ -1,6 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-const { post } = require('../routes');
 
 class Vote extends Model {};
 
--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,12 +1,19 @@
 const router = require('express').Router();
-const { Post, User } = require('../../models');
+const { Post, User, Vote } = require('../../models');
+const sequelize = require('../../config/connection')
 
 // get all users
 router.get('/', (req, res) => {
     console.log('=================')
     Post.findAll({
         // query
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id', 
+            'post_url', 
+            'title', 
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.user_id)'), 'vote_count']
+        ],
         order: [['created_at', 'DESC']],
         include: [
             {
@@ -27,7 +34,13 @@ router.get('/:id', (req, res) => {
         where: {
             id: req.params.id,
         },
-        attributes: ['id', 'post_url', 'title', 'created_at'],
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+        ],
         include: [
             {
                 model: User,
@@ -60,6 +73,15 @@ router.post('/', (req, res) => {
         res.status(500).json(err);
     });
 });
+
+router.put('/upvote', (req, res) => {
+    Post.upvote(req.body, { Vote })
+    .then(dbPostData => res.json(dbPostData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+    })
+})
 
 router.put('/:id', (req, res) => {
     Post.update(
